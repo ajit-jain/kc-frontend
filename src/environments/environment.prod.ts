2ee60e903b16306a8f210964b094baee42fa7fd9
ajit-jain <jainajit194@gmail.com>
2018-12-09 17:55:58 +0530

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API: 'https://git.heroku.com/kc-backend.git/api/v1',
+  API: 'https://kc-backend.herokuapp.com/api/v1',
   ADMIN_NUMBER: '+917009104362'
 
 };
