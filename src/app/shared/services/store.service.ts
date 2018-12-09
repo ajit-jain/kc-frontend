13f6751048a2b1447c36b4ec8ede6d16a95355e9
ajit-jain <jainajit194@gmail.com>
2018-12-09 17:37:59 +0530

    changes

--- src/app/shared/services/store.service.ts
+++ src/app/shared/services/store.service.ts
@@ -1,5 +1,5 @@
 import { environment } from './../../../environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
@@ -8,7 +8,12 @@ import { throwError } from 'rxjs';
 })
 export class StoreService {
   api = environment.API;
-  constructor(private http: HttpClient) { }
+  headers;
+  constructor(private http: HttpClient) {
+    this.headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+  }
   getContacts() {
     return this.http.get('./assets/default.json').pipe(
       catchError((res: Response) => {
@@ -17,14 +22,14 @@ export class StoreService {
     );
   }
   sendMessage(data) {
-    return this.http.post(`${this.api}/message`, data).pipe(
+    return this.http.post(`${this.api}/message`, data, { headers: this.headers }).pipe(
       catchError((res: Response) => {
         return throwError(res['error']);
       })
     );
   }
   getMessages(pageNo, limit) {
-    return this.http.get(`${this.api}/messages?page=${pageNo}&limit=${limit}`).pipe(
+    return this.http.get(`${this.api}/messages?page=${pageNo}&limit=${limit}`, { headers: this.headers }).pipe(
       catchError((res: Response) => {
         return throwError(res['error']);
       })
