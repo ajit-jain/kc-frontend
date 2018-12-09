7f6e8e9ae0f6bac294624f0c2c89d5bb4315cfc7
ajit-jain <jainajit194@gmail.com>
2018-12-09 16:30:03 +0530

    changes

--- src/app/shared/services/store.service.ts
+++ src/app/shared/services/store.service.ts
@@ -7,7 +7,7 @@ import { throwError } from 'rxjs';
   providedIn: 'root'
 })
 export class StoreService {
-  api = environment.API;
+  api = 'http://ghserver.in:4800/api/v1';
   constructor(private http: HttpClient) { }
   getContacts() {
     return this.http.get('./assets/default.json').pipe(
